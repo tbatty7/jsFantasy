bef4b448bcbb90c3de1e93b9741e95e100319877
Timothy Batty <tbatty7@gmail.com>
2017-07-15 01:28:28 -0400

    Split PLAYER_LIST from SOCKET_LIST to set up for controlling players and created object constructor for players

--- app.js
+++ app.js
@@ -20,36 +20,52 @@ console.log("Server Started");
 // while coding communication on the server.
 
 var SOCKET_LIST = {};
-
+// Here we are creating a new object called the PLAYER_LIST.
+var PLAYER_LIST = {};
+// Now we are adding a player constrictor with the function below.
+var Player = function(id) { //This will create a player with this id
+	var self = {
+		x: 250,
+		y: 250,
+		id: id,  //You have to pass the id in this function
+		number: " " + Math.floor(10 * Math.random())
+	}
+	return self;
+}
 var io = require('socket.io')(serv,{});
 
 io.sockets.on('connection', function(socket) {
 	socket.id = Math.random();
-	socket.x = 0;
-	socket.y = 0;
-	socket.number = ' ' + Math.floor(10 * Math.random());
 	SOCKET_LIST[socket.id] = socket;
+
+	var player = Player(socket.id);
+	PLAYER_LIST[socket.id] = player;
 
 	socket.on("disconnect", function() {
 		delete SOCKET_LIST[socket.id];
 		//This disconnects the player when they close their browser.
+		delete PLAYER_LIST[socket.id];
 	});
 	socket.emit('serverMsg', {
 		msg: 'hello'
 	});
 
 });
-
+// To allow the player to interact with the character
+// you need to seperate the sockets and players in the 
+// SOCKET_LIST.  You cannot have the x and y position
+// directly linked with the socket if you want to control
+// your character.  
 setInterval(function(){
 	var pack = [];
-	for (var i in SOCKET_LIST) {
-		var socket = SOCKET_LIST[i];
-		socket.x++;
-		socket.y++;
+	for (var i in PLAYER_LIST) {
+		var player = PLAYER_LIST[i];
+		player.x++;
+		player.y++;
 		pack.push({
-			x:socket.x,
-			y:socket.y,
-			number:socket.number
+			x:player.x,
+			y:player.y,
+			number:player.number
 		});
 	}
 	for (var i in SOCKET_LIST){

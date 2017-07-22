32faabaef742388393f53d9c5dad45e1d0133ea8
Timothy Batty <tbatty7@gmail.com>
2017-07-22 13:47:38 -0400

    Move all js to jScript.js file

--- client/js/jScript.js
+++ client/js/jScript.js
@@ -1,25 +1,63 @@
 $(function(){
 
-	$('button#submitCon').click(function(event){
-		event.preventDefault();
-		var consoleText = $('textarea#console').val();
-		// console.log(consoleText);
-		$('#lesson').prepend('<p>' + consoleText + '</p>');
-		socket.emit('consoleText', consoleText);
-		$('textarea#console').val("");
-	});
+	oldButton();
+	bigConsole();
+	consoleEnter();
+});
 
-	$('input#submitBigCon').click(function(event){
-		event.preventDefault();
-		var bigConsoleText = $('textarea#bigConsole').val();
-		console.log(bigConsoleText);
-		$('#lesson').prepend('<p>' + bigConsoleText + '</p>');
-		socket.emit('bigConsoleText', bigConsoleText);
-		$('textarea#bigConsole').val("");
-		// return bigConsoleText;
 
-	});
 
+var ctx = document.getElementById("ctx").getContext("2d");
+ctx.font = '30px Arial';
+
+var socket = io();
+
+socket.on('newPositions', function(data) {
+	ctx.clearRect(0, 0, 700, 400);
+	for (var i = 0; i < data.length; i++) 
+		ctx.fillText(data[i].number, data[i].x, data[i].y);
+
+
+});
+function east(num) {
+	if (num < 1) {
+		socket.emit('newPositions', {direction:'east', endPosition: 1});
+	} else if (!num) {
+		socket.emit('newPositions', {direction:'east', endPosition: 1});
+	} else {
+		socket.emit('newPositions', {direction:'east', endPosition: num});
+	}
+
+}
+function west(num) {
+	if (num < 1) {
+		socket.emit('newPositions', {direction:'west', endPosition: 1});
+	} else if (!num) {
+		socket.emit('newPositions', {direction:'west', endPosition: 1});
+	} else {
+		socket.emit('newPositions', {direction:'west', endPosition: num});
+	}
+}
+function north(num) {
+	if (num < 1) {
+		socket.emit('newPositions', {direction:'north', endPosition: 1});
+	} else if (!num) {
+		socket.emit('newPositions', {direction:'north', endPosition: 1});
+	} else {
+		socket.emit('newPositions', {direction:'north', endPosition: num});
+	}
+}
+function south(num) {
+	if (num < 1) {
+		socket.emit('newPositions', {direction:'south', endPosition: 1});
+	} else if (!num) {
+		socket.emit('newPositions', {direction:'south', endPosition: 1});
+	} else {
+		socket.emit('newPositions', {direction:'south', endPosition: num});
+	}
+}
+
+function consoleEnter() {
 	$( "textarea#console" ).keydown(function( event ) {
 	  if ( event.keyCode === 13 ) {  // Enter key
 		  	// var consoleText = document.getElementById('console').value;
@@ -38,20 +76,39 @@ $(function(){
 		  	} else if (!consoleText){
 		  		alert('Please Enter Text in Console below Video Screen before hitting Enter');
 		  	}
-	  
-		
-		
 		} 
-		
+	});
+}
+
+function bigConsole() {
+	$('input#submitBigCon').click(function(event){
+		event.preventDefault(); // stops form from refreshing screen
+		var bigConsoleText = $('textarea#bigConsole').val();
+		console.log(bigConsoleText);
+		$('#lesson').prepend('<p>' + bigConsoleText + '</p>');
+		socket.emit('bigConsoleText', bigConsoleText);
+		$('textarea#bigConsole').val("");
+		// return bigConsoleText;
+
+	});
+}
+
+function oldButton() {
+	$('button#submitCon').click(function(event){
+		event.preventDefault();
+		var consoleText = $('textarea#console').val();
+		// console.log(consoleText);
+		$('#lesson').prepend('<p>' + consoleText + '</p>');
+		socket.emit('consoleText', consoleText);
+		$('textarea#console').val("");
 	});
 
+}
 
 
-
-});
-
-// function handle(event){
-//         event.preventDefault(); // Otherwise the form will be submitted
-
-//         alert("FORM WAS SUBMITTED WITH ENTER");
-//     }
+module.exports = {
+	south:south,
+	north:north,
+	east:east,
+	west:west
+}

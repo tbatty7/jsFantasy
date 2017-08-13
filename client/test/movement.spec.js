3ed49b4c81f9746045cc04e40617363bfeea9db6
Timothy Batty <tbatty7@gmail.com>
2017-08-12 22:36:53 -0400

    Changed setup of sinon to stub out socket emit function instead of spy, added emit.restore() afterEach.  Tests work now.

--- client/test/movement.spec.js
+++ client/test/movement.spec.js
@@ -1,8 +1,6 @@
 var expect = chai.expect;
-
-socket = {
-    emit: sinon.spy()
-}
+var socket = { emit: function(){}};
+var emit;
 
 describe('basic tests', function () {
 
@@ -13,37 +11,72 @@ describe('basic tests', function () {
 });
 
 describe('movement tests', function () {
+    beforeEach(function(){
+
+        emit = sinon.stub(socket, 'emit');
+    });
+
+    afterEach(function(){
+        emit.restore();
+    });
 
     it('should emit a movement event for east when greater than 0', function() {
         east(10);
-        expect(socket.emit).to.be.calledWith('newPositions', {direction: 'east', endPosition: 10});
+        expect(emit).to.be.calledWith('newPositions', {direction: 'east', endPosition: 10});
     });
 
     it('should emit a movement event for east when less than 1', function() {
         east(-10);
-        expect(socket.emit).to.be.calledWith('newPositions', {direction: 'east', endPosition: 1});
-    });
+        expect(emit).to.be.calledWith('newPositions', {direction: 'east', endPosition: 1});
+     });
 
     it('should emit a movement event for east when not a number', function() {
         east("ward");
-        expect(socket.emit).to.be.calledWith('newPositions', {direction: 'east', endPosition: 1});
+        expect(emit).to.be.calledWith('newPositions', {direction: 'east', endPosition: 1});
     });
-});
 
-describe('movement tests', function () {
-
-    it('should emit a movement event for east when greater than 0', function() {
+    it('should emit a movement event for west when greater than 0', function() {
         west(10);
-        expect(socket.emit).to.be.calledWith('newPositions', {direction: 'west', endPosition: 10});
+        expect(emit).to.be.calledWith('newPositions', {direction: 'west', endPosition: 10});
     });
 
-    it('should emit a movement event for east when less than 1', function() {
+    it('should emit a movement event for west when less than 1', function() {
         west(-10);
-        expect(socket.emit).to.be.calledWith('newPositions', {direction: 'west', endPosition: 1});
+        expect(emit).to.be.calledWith('newPositions', {direction: 'west', endPosition: 1});
     });
 
-    it('should emit a movement event for east when not a number', function() {
+    it('should emit a movement event for west when not a number', function() {
         west("ward");
-        expect(socket.emit).to.be.calledWith('newPositions', {direction: 'west', endPosition: 1});
+        expect(emit).to.be.calledWith('newPositions', {direction: 'west', endPosition: 1});
+    });
+
+    it('should emit a movement event for north when greater than 0', function() {
+        north(10);
+        expect(emit).to.be.calledWith('newPositions', {direction: 'north', endPosition: 10});
+    });
+
+    it('should emit a movement event for north when less than 1', function() {
+        north(-10);
+        expect(emit).to.be.calledWith('newPositions', {direction: 'north', endPosition: 1});
+    });
+
+    it('should emit a movement event for north when not a number', function() {
+        north("ward");
+        expect(emit).to.be.calledWith('newPositions', {direction: 'north', endPosition: 1});
+    });
+
+    it('should emit a movement event for south when greater than 0', function() {
+        south(10);
+        expect(emit).to.be.calledWith('newPositions', {direction: 'south', endPosition: 10});
+    });
+
+    it('should emit a movement event for south when less than 1', function() {
+        south(-10);
+        expect(emit).to.be.calledWith('newPositions', {direction: 'south', endPosition: 1});
+    });
+
+    it('should emit a movement event for south when not a number', function() {
+        south("ward");
+        expect(emit).to.be.calledWith('newPositions', {direction: 'south', endPosition: 1});
     });
 });
